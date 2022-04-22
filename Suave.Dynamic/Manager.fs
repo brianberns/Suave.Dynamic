b4931ccf230f803eb4bb9a3ea46a10398c133da5
Brian Berns <brianberns@gmail.com>
2022-04-21 21:37:50 -0400

    Work on combining web parts dynamically.

--- Suave.Dynamic/Manager.fs
+++ Suave.Dynamic/Manager.fs
@@ -4,30 +4,34 @@ open System.IO
 open System.Reflection
 
 open Suave
+open Suave.Filters
+open Suave.Operators
+
 open Tommy
 
 module Manager =
 
-    let create path =
-        use reader = new StreamReader(path : string)
+    let create tomlPath =
+        use reader = new StreamReader(tomlPath : string)
         let table = TOML.Parse(reader)
-        let parts = table["web_part"]
-        for key in parts.Keys do
-            let subtable = parts[key].AsTable
-            let filePath = subtable["file_path"].AsString.Value
-            let webPath = subtable["web_path"].AsString.Value
-            let assembly =
-                filePath
-                    |> Path.GetFullPath
-                    |> Assembly.LoadFile
-            let prop =
-                seq {
-                    for typ in assembly.GetTypes() do
-                        for prop in typ.GetProperties(BindingFlags.Static ||| BindingFlags.Public) do
-                            if prop.PropertyType = typeof<WebPart> then
-                                yield prop
-                } |> Seq.exactlyOne
-            let part = prop.GetMethod.Invoke(null, Array.empty) :?> WebPart
-            ()
-        
-        Successful.OK "Hello World!"
+        let partsNode = table["web_part"]
+        choose [
+            for key in partsNode.Keys do
+                let subtable = partsNode[key].AsTable
+                let filePath = subtable["file_path"].AsString.Value
+                let webPath = subtable["web_path"].AsString.Value
+                let assembly =
+                    filePath
+                        |> Path.GetFullPath
+                        |> Assembly.LoadFile
+                let prop =
+                    seq {
+                        for typ in assembly.GetTypes() do
+                            for prop in typ.GetProperties(BindingFlags.Static ||| BindingFlags.Public) do
+                                if prop.PropertyType = typeof<WebPart> then
+                                    yield prop
+                    } |> Seq.exactlyOne
+                let part = prop.GetMethod.Invoke(null, Array.empty) :?> WebPart
+                yield path webPath >=> part
+            yield RequestErrors.NOT_FOUND "Found no handlers."
+        ]
--- Test/TestHarness/Program.fs
+++ Test/TestHarness/Program.fs
@@ -1,5 +1,15 @@
 ﻿open Suave
 open Suave.Dynamic
+open Suave.Filters
+open Suave.Logging
+open Suave.Operators
 
-let app = Manager.create "WebParts.toml"
+let app =
+    let logger = Targets.create LogLevel.Info [||]
+    Manager.create "WebParts.toml"
+        >=> logWithLevelStructured
+            LogLevel.Info
+            logger
+            logFormatStructured
+
 startWebServer defaultConfig app
